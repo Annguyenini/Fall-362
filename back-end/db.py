39e42b150311fc9d858b6ed5a61d239599d154d9
Sean <scannon@protonmail.com>
2026-09-23 20:09:57 -0700

    db.py for easy db connection, items route, dotenv/psycop requirements, gitignore for venv, gitattributes for a docker compose fix

---
+++ back-end/db.py
@@ -0,0 +1,16 @@
+import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
+#read env and load into connection,dictcursor for formatting
+load_dotenv()
+
+def get_connection():
+    return psycopg2.connect(
+        host="127.0.0.1",
+        port=5435,
+        dbname=os.getenv("DATABASE_NAME"),
+        user=os.getenv("DATABASE_USER"),
+        password=os.getenv("DATABASE_PASS"),
+        cursor_factory=RealDictCursor,
+    )
--- back-end/main.py
+++ back-end/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from db import get_connection
 
 app = FastAPI()
 
@@ -6,3 +7,15 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+#open database connection,pull every row from items table,return as JSON
+#try block to guarentee connection close
+@app.get("/items")
+def get_items():
+    conn = get_connection()
+    try:
+        with conn.cursor() as cur:
+            cur.execute("SELECT sku,title,price FROM items ORDER BY sku;")
+            return cur.fetchall()
+    finally:
+        conn.close()
