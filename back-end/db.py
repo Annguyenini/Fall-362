5492fe2faadd04105de5464c10cbb1a943481930
Sean <scannon@protonmail.com>
2026-09-23 22:37:54 -0700

    forgot parenthesis

--- back-end/db.py
+++ back-end/db.py
@@ -8,7 +8,7 @@ load_dotenv()
 def get_connection():
     return psycopg2.connect(
         host=os.getenv("DATABASE_HOST"),
-        port=os.getenv(DATABASE_PORT),
+        port=os.getenv("DATABASE_PORT"),
         dbname=os.getenv("DATABASE_NAME"),
         user=os.getenv("DATABASE_USER"),
         password=os.getenv("DATABASE_PASS"),
