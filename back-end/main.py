32f087b2e7e204d149d15fbc7184cd7ae1d5005b
Sean <scannon@protonmail.com>
2026-09-25 21:26:21 -0700

    add inventory to inventories/get inventories

--- back-end/main.py
+++ back-end/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
 from db import get_connection
+import uuid
 
 app = FastAPI()
+
+# using pydantic model for req body shape(make sure location get sent)
+class InventoryCreate(BaseModel):
+    location: str
 
 
 @app.get("/")
@@ -32,5 +38,31 @@ def get_items(sku):
                 raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                                     detail = f"Item with sku: {sku} does not exist")
             return item
+    finally:
+        conn.close()
+
+#pass in location to generate new uuid and add a new inventory, returns new id and location
+@app.post("/inventories",status_code=status.HTTP_201_CREATED)
+def create_inventory(body: InventoryCreate):
+    new_id = str(uuid.uuid4())
+    conn = get_connection()
+    try:
+        with conn.cursor() as cur:
+            cur.execute(
+                "INSERT INTO inventories (inventory_id, location) VALUES (%s, %s);", (new_id, body.location),
+            )
+        conn.commit()
+        return {"inventory_id": new_id, "location": body.location}
+    finally:
+        conn.close()
+
+#return all inventories for front end buttons
+@app.get("/inventories")
+def get_inventories():
+    conn = get_connection()
+    try:
+        with conn.cursor() as cur:
+            cur.execute("SELECT inventory_id, location FROM inventories ORDER BY location;")
+            return cur.fetchall()
     finally:
         conn.close()
